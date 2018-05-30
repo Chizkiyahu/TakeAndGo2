b9a6b68986c60a6525e669365336dcfee9cea444
chezkiaho <chezkiaho@gmail.com>
2018-05-30 20:58:48 +0300

    fix menu

--- app/src/main/java/com/example/chizkiyahuandchaskyh/takeandgo2/controller/MainActivity.java
+++ app/src/main/java/com/example/chizkiyahuandchaskyh/takeandgo2/controller/MainActivity.java
@@ -95,13 +95,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.contact_us) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.branches) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.active_invitation) {
+
+        } else if (id == R.id.log_out) {
 
         } else if (id == R.id.nav_share) {
 
