f7217da4c2b0845a0ea5ca0174ae34f3b70df874
Chizkiyahu Raful <ChizkiyahuRaful@gmail.com>
2018-06-11 00:58:30 +0300

    update ContactUsFragment

--- app/src/main/java/com/example/chizkiyahuandchaskyh/takeandgo2/controller/main/MainActivity.java
+++ app/src/main/java/com/example/chizkiyahuandchaskyh/takeandgo2/controller/main/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        checkLogin();
+        //checkLogin();
         if (contactUsFragment == null){
             contactUsFragment = new ContactUsFragment();
         }
