95d054fa42e849dc0d22dce291717578c57d711f
Chizkiyahu Raful <ChizkiyahuRaful@gmail.com>
2018-06-11 01:00:24 +0300

    update ContactUsFragment

--- app/src/main/java/com/example/chizkiyahuandchaskyh/takeandgo2/controller/main/MainActivity.java
+++ app/src/main/java/com/example/chizkiyahuandchaskyh/takeandgo2/controller/main/MainActivity.java
@@ -53,7 +53,6 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         checkLogin();
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
